4abf4634d797c8f088cc231b8b810a7a3e094c0d
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2017-11-03 11:03:59 +0100

    Commit para subir

---
+++ Boletin5.7/src/boletin5/pkg7/Boletin57.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5.pkg7;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Boletin57 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean opcionNoElegida = false;
+        do{
+        String res = JOptionPane.showInputDialog(null, "MENU\nElija una opción:\n1)Cadrado\n2)Triángulo\n3)Circulo");
+        switch(Integer.parseInt(res)){
+            case 1:
+                JOptionPane.showMessageDialog(null,"Ha elegido el cuadrado");
+                Cuadrado ejemplo = new Cuadrado();
+                ejemplo.introducirLado();
+                ejemplo.calcularArea();
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null,"Ha elegido el triangulo");
+                Triangulo ejemplo2 = new Triangulo();
+                ejemplo2.introducirBaseAltura();
+                ejemplo2.calcularArea();
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null,"Ha elegido el circulo");
+                Circulo ejemplo3 = new Circulo();
+                ejemplo3.introducirRadio();
+                ejemplo3.calcularArea();
+                break;
+            default:JOptionPane.showMessageDialog(null,"Opcion incorrecta");
+                opcionNoElegida = true;
+        }
+        }while(opcionNoElegida);
+        
+        
+        
+}
+        
+        
+    }
+    
+
