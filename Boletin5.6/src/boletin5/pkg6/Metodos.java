0537ce4bdde6312c29ca7064e5340eb14b820058
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2017-11-03 10:52:44 +0100

    Commit para subir

---
+++ Boletin5.6/src/boletin5/pkg6/Metodos.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5.pkg6;
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Metodos {
+    String nome;
+    int ventas;
+
+    public Metodos() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVentas() {
+        return ventas;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setVentas(int ventas) {
+        this.ventas = ventas;
+    }
+    
+    
+    
+}
